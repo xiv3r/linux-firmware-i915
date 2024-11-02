1425749aab879621c8f9fe0738f1b9c9f002f092
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-11-02 11:45:48 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Install the latest i915 firmware
+sudo rm -rf /lib/firmware/i915
+wget -r -l 1 -A "*.bin" --no-parent -e robots=off -U Mozilla https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git/plain/i915/
+sudo mv git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git/plain/i915 /lib/firmware/
+sudo rm -rf git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git/plain/
+
+## Update the initramfs
+sudo update-initramfs -u -k all
