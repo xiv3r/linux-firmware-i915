bd34eb7bce0a821a506ebb09b63f9654b00f457b
𝑹𝒂𝒇𝒇 𝑨𝒍𝒅𝒔 <117867334+xiv3r@users.noreply.github.com>
2026-02-22 16:26:12 +0800

    Fix

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 ###
-wget -O 
-rm /lib/firmware/i915
-mv linux-firmware/i915/* /lib/firmware/i915/
+wget -O firmware.zip https://github.com/xiv3r/linux-firmware-i915/releases/download/firmware-i915/firmware.zip
+
+rm /lib/firmware/i915/*
+
+unzip -o firmware.zip -d /lib/firmware/i915
 
